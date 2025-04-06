2303bd2823db969482d4ef7c87d3543aba29c7f4
gargi <gargi.p.sukhatankar@gmail.com>
2025-04-06 15:01:01 +0530

    updated model

--- models/EeveeModel.jsx
+++ models/EeveeModel.jsx
@@ -1,14 +1,26 @@
-
-import React, { useRef, useEffect } from 'react';
+"use client";
+import React, { useState, useRef, useEffect } from 'react';
 import { useGLTF } from '@react-three/drei';
 import { useFrame, useThree } from '@react-three/fiber';
 
 const EeveeModel = (props) => {
+
+  const [isMobileScreenSize, setIsMobileScreenSize] = useState(false);
   const { nodes, materials } = useGLTF('/eevee.glb');
   const eeveeRef = useRef();
+
+  useEffect(()=>{
+    const updateScreensize = () => {
+        setIsMobileScreenSize(window.innerWidth<768);
+    }
+    updateScreensize();
+    window.addEventListener("resize", updateScreensize);
+    return () => window.removeEventListener(resizeBy, updateScreensize);
+  }, []);
+
   return (
     <group ref={eeveeRef} {...props}>
-      <group scale={0.1}>
+      <group scale={isMobileScreenSize ? 0.018 : 0.028}>
         <mesh
           geometry={nodes.Eevee_Material__12_0.geometry}
           material={materials.Material_12}
