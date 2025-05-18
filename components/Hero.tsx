ad5cf1af1b457d9f20f865212b36aa214e8e5c35
gargi <gargi.p.sukhatankar@gmail.com>
2025-05-18 14:07:05 +0530

    updated

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { cn } from "@/lib/utils";
 import { Spotlight } from "@/components/ui/Spotlight";
-import { TextGenerateEffect } from './ui/Text-generate-effect';
+import { TextGenerateEffect } from './ui/text-generate-effect';
 import MagicButton from './ui/MagicButton';
 import { div } from 'motion/react-client';
 import { FaBeer, FaLocationArrow } from 'react-icons/fa';
 import Eevee from '../components/Eevee';
 const Hero = () => {
   return (
-    <div className='pb-40 pt-36'>
+    <div className=''>
 
       <div>
         <Spotlight className='-top-10 -right-11 h-[80vh] w-[50vw]' fill="purple" />
@@ -19,7 +19,7 @@ const Hero = () => {
       </div>
       <div>
 
-        <div className="absolute md:top-0 md:left-0 flex flex-col overflow-hidden md:flex-row justify-center md:h-screen w-screen items-center bg-white dark:bg-black-100">
+        <div className="md:top-0 md:left-0 flex flex-col overflow-hidden md:flex-row justify-center md:h-screen w-screen items-center bg-white dark:bg-black-100">
           <div
             className={cn(
               "absolute inset-0",
@@ -31,7 +31,7 @@ const Hero = () => {
 
 
           {/* Radial gradient for the container to give a faded look */}
-          <div className="pointer-events-none absolute inset-0 flex  items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100" />
+          <div className="pointer-events-none  inset-0 flex  items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100" />
 
           <div className='flex justify-center relative z-10 my-20'>
             <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center'>
