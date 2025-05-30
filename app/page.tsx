c34073a07caaa4c9f031605afc25aa35fd3fa4b2
gargi <gargi.p.sukhatankar@gmail.com>
2025-05-30 20:41:26 +0530

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,16 @@ import { FaHome } from "react-icons/fa";
 import {BentoGridDemo}from "@/components/Grid";
 export default function Home() {
   return (
-    <main className="relative bg-black-100 mx-auto flex  flex-col justify-center items-center overflow-hiddensm:px-10 px-5">
+    <main className="relative bg-black-100 mx-auto flex flex-col justify-center items-center overflow-hiddensm:px-10 px-5">
       <div className="max-w-7xl w-full">
       <FloatingNav navItems={[
         {name: 'Home', link: '/', icon: <FaHome/>}
       ]}/>
-    
+  
+  <div className="flex flex-col justify-center items-center gap-0">
       <Hero/>
-      <BentoGridDemo/>
+      {/* <BentoGridDemo/> */}
+        </div>
       </div>
     </main>
   );
