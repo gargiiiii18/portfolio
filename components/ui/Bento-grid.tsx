d3677476a565cadaead53802fef4497eedd44ffd
gargi <gargi.p.sukhatankar@gmail.com>
2025-05-30 20:41:45 +0530

    last stable checkpoint

--- components/ui/Bento-grid.tsx
+++ components/ui/Bento-grid.tsx
@@ -31,11 +31,12 @@ export const BentoGridItem = ({
   description?: string | React.ReactNode;
   header?: React.ReactNode;
   icon?: React.ReactNode;
+  img?: string | React.ReactNode;
 }) => {
   return (
     <div
       className={cn(
-        "group/bento shadow-input row-span-1 flex flex-col justify-between space-y-4 rounded-xl border border-neutral-200 bg-white p-4 transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none",
+        "group/bento shadow-input row-span-1 flex flex-col justify-between space-y-4 rounded-xl border border-neutral-200 bg-white mx-8 md:mx-0 p-4 transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none",
         className,
       )}
     >
