a611d18c1d5bdfb0a415b0af7940532bdf3c5fac
gargi <gargi.p.sukhatankar@gmail.com>
2025-06-02 01:46:52 +0530

    updated

--- data/index.js
+++ data/index.js
@@ -23,7 +23,7 @@ export const gridItems = [
   {
     id: 1,
     title: "I prioritize client collaboration, fostering open communication ",
-    description: "",
+    description: "I constantly try to improve",
     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
     imgClassName: "w-full h-full",
     titleClassName: "justify-end",
@@ -33,11 +33,11 @@ export const gridItems = [
   {
     id: 2,
     title: "I'm very flexible with time zone communications",
-    description: "",
+    description: "hehee",
     className: "lg:col-span-2 md:col-span-3 md:row-span-2",
     imgClassName: "",
     titleClassName: "justify-start",
-    img: "",
+    img: "/b5.svg",
     spareImg: "",
   },
   {
@@ -47,7 +47,7 @@ export const gridItems = [
     className: "lg:col-span-2 md:col-span-3 md:row-span-2",
     imgClassName: "",
     titleClassName: "justify-center",
-    img: "",
+    img: "/b4.svg",
     spareImg: "",
   },
   {
@@ -65,10 +65,10 @@ export const gridItems = [
     id: 5,
     title: "Currently building a JS Animation library",
     description: "The Inside Scoop",
-    className: "md:col-span-3 md:row-span-2",
-    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    className: "md:col-span-3 md:row-span-1",
+    imgClassName: "",
     titleClassName: "justify-center md:justify-start lg:justify-center",
-    img: "/b5.svg",
+    img: "",
     spareImg: "/grid.svg",
   },
   {
@@ -77,9 +77,9 @@ export const gridItems = [
     description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
     imgClassName: "",
-    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center text-black",
     img: "",
-    spareImg: "",
+    spareImg: "/b4.svg",
   },
 ];
 
