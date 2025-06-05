2ca3803f7564b39604051ec446e55736f0bdd8f3
gargi <gargi.p.sukhatankar@gmail.com>
2025-06-05 21:58:21 +0530

    updated

--- data/index.js
+++ data/index.js
@@ -22,8 +22,8 @@ export const navItems = [
 export const gridItems = [
   {
     id: 1,
-    title: "I prioritize client collaboration, fostering open communication ",
-    description: "I constantly try to improve",
+    title: "BTech. in Computer Science & Engineering (Internet of Things, Cybersecurity and Blockchain Technology)",
+    description: "I am a Final Year student pursuing",
     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
     imgClassName: "w-full h-full",
     titleClassName: "justify-end",
@@ -75,8 +75,8 @@ export const gridItems = [
   {
     id: 6,
     title: "Do you want to start a project together?",
-    description: "",
-    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    description: "Open to Collaborations",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-3",
     imgClassName: "",
     titleClassName: "justify-center md:max-w-full max-w-60 text-center text-black",
     img: "",
