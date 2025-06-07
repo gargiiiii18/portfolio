927ecc0e5881f798874e2235d117b7cd05c885b9
gargi <gargi.p.sukhatankar@gmail.com>
2025-06-08 00:26:52 +0530

    updated page

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { FloatingNav } from "@/components/ui/Floating-navbar";
 import { FaHome } from "react-icons/fa";
 import {BentoGridDemo}from "@/components/Grid";
 import { GlobeDemo } from "@/components/GridGlobe";
+import { Projects } from "@/components/Projects";
 export default function Home() {
   return (
     <main className="relative overflow-hidden bg-black-100 mx-auto flex flex-col justify-center items-center overflow-hiddensm:px-10 px-5">
@@ -15,12 +16,13 @@ export default function Home() {
     <div className="-top-60 md:-top-30 absolute w-full h-full">
         <GlobeDemo/>
         </div>
-  <div className="z-10 flex flex-col justify-center items-center gap-0">
+        <div className="z-10 flex flex-col justify-center items-center gap-0">
     
       
       <Hero/>
       {/* <BentoGridDemo/> */}
         </div>
+      <Projects/>  
       </div>
     </main>
   );
