7f23b823d6dd8ec1fb8016de41840497f713aada
gargi <gargi.p.sukhatankar@gmail.com>
2025-06-08 00:26:48 +0530

    updated page

---
+++ components/Projects.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Projects = () => {
+  return (
+    <div className='py-20'>
+       <h1 className='heading text-center text-xl md:text-2xl lg:text-3xl'>
+        Few of My {' '}
+        <span className='text-purple-300'>Projects</span>
+       </h1>
+       <div className='flex flex-wrap justify-center p-4 gap-16 mt-10 items-center'>
+
+       </div>
+        </div>
+  )
+}
