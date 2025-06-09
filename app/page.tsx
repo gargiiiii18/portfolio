6b7a4caf0b19826778b412cf27e262f75f4a2276
gargi <gargi.p.sukhatankar@gmail.com>
2025-06-10 01:04:58 +0530

    added navigation

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,14 @@ import { FaHome } from "react-icons/fa";
 import {BentoGridDemo}from "@/components/Grid";
 import { GlobeDemo } from "@/components/GridGlobe";
 import { Projects } from "@/components/Projects";
+import { navItems } from "@/data";
 export default function Home() {
   return (
     <main className="relative overflow-hidden bg-black-100 mx-auto flex flex-col justify-center items-center overflow-hiddensm:px-10 px-5">
 
 
       <div className="relative z-10 max-w-7xl w-full">
-      <FloatingNav navItems={[
-        {name: 'Home', link: '/', icon: <FaHome/>}
-      ]}/>
+      <FloatingNav navItems={navItems}/>
     <div className="-top-60 md:-top-30 absolute w-full h-full">
         <GlobeDemo/>
         </div>
