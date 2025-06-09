3733cb6ae358f6aad074aba55e81e3d1c01d3bd0
gargi <gargi.p.sukhatankar@gmail.com>
2025-06-10 01:05:37 +0530

    updated

--- data/index.js
+++ data/index.js
@@ -15,7 +15,6 @@ const Skeleton = () => (
 export const navItems = [
   { name: "About", link: "#about" },
   { name: "Projects", link: "#projects" },
-  // { name: "Testimonials", link: "#testimonials" },
   { name: "Contact", link: "#contact" },
 ];
 
