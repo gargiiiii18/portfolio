ceb01a0d3b0beec470cdb379762823ee6b49d0a0
gargi <gargi.p.sukhatankar@gmail.com>
2025-06-12 00:48:37 +0530

    updated

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -16,13 +16,14 @@ export function BentoGridDemo() {
   
   return (
     <BentoGrid className="max-w-4xl h-full mx-auto z-20">
-      {gridItems.map(({title, description, img, id, className, spareImg, imgClassName, titleClassName}, i) => (
+      {gridItems.map(({title, description, img, id, className, spareImg, imgClassName, desc2, titleClassName}, i) => (
         
         <BentoGridItem
           id={id}
           key={i}
           title={title}
           description={description}
+          desc2={desc2}
           img = {img}
           // header={item.header}
           // icon={item.icon}
