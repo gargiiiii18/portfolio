9272b62531ae109f8adb25ec6cd36e6acd6d24ef
gargi <gargi.p.sukhatankar@gmail.com>
2025-06-17 02:08:51 +0530

    added snake component

---
+++ components/ui/Snake.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Snake = () => {
+  return (
+    <div>
+        <img
+  src="https://raw.githubusercontent.com/gargiiiii18/gargiiiii18/output/github-snake-dark.svg"
+  alt="GitHub contribution snake"
+  className="w-full"
+/>
+    </div>
+  )
+}
