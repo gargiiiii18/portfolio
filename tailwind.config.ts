fe42cef2924d3482aa4c957643094acc6d520825
gargi <gargi.p.sukhatankar@gmail.com>
2025-06-19 01:09:36 +0530

    updated

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import  type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        typewriter: ['var(--font-typewriter)', 'monospace'],
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
