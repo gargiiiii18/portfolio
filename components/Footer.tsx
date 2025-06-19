e441a392e116008f1efb1b069493344ec6bb547e
gargi <gargi.p.sukhatankar@gmail.com>
2025-06-19 19:38:26 +0530

    resolved errors

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import MagicButton from './ui/MagicButton'
 import { FaLocationArrow } from 'react-icons/fa'
 import { socialMedia } from '@/data';
+import Image from 'next/image';
 
 export const Footer = () => {
 
