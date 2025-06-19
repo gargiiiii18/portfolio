691344031c1ccc29944c62652e73447c35910d07
gargi <gargi.p.sukhatankar@gmail.com>
2025-06-19 20:06:58 +0530

    final commit

--- components/ui/Orbit.tsx
+++ components/ui/Orbit.tsx
@@ -21,11 +21,11 @@ export const Orbit = ({
 
     const centerClass = "absolute top-1/2 left-1/2";
 
-    const adjustedRadius = radius ?? (isMobile ? 60 : 90);
+    const adjustedRadius = radius ?? (isMobile ? 50 : 90);
 
   return (
     <div className='flex justify-center items-center'>
-    <div className='absolute -top-21 -right-13 sm:-top-19 sm:-right-8 md:-right-5 md:-top-5 md:mr-4 mx-auto h-[300px] w-[300px]'>
+    <div className='absolute -top-21 -right-18 sm:-top-19 sm:-right-8 md:-right-5 md:-top-5 md:mr-4 mx-auto h-[300px] w-[300px]'>
         {technologiesImgs.map(({id, title, img}, index) => {
 
             const angle = (2 * Math.PI / technologiesImgs.length) * index;
